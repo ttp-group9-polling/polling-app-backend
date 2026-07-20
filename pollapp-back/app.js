abbdbae9b1c5ba37995510fbb03d1a7db27c1ad5
Rehman <rehmanmohammad938@gmail.com>
2026-07-20 12:04:42 -0400

    First Commit

---
+++ pollapp-back/app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const PORT = 3000;
+const app = express();
+
+app.use(express.json);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on Port ${PORT}`);
+});
